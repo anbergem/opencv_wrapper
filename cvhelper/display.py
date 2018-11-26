03e70cfa7e5cc5dc3ebdc5b35c9c5b757c359bd9
Andreas Bergem <bergem.andreas@gmail.com>
2018-11-26 12:21:31 +0100

    Add line and circle for display.

--- cvhelper/display.py
+++ cvhelper/display.py
@@ -1,4 +1,4 @@
-from typing import Union, Tuple
+from typing import Union, Tuple, Optional
 
 import cv2 as cv
 import numpy as np
@@ -7,10 +7,30 @@ from .model import Point, Rect
 from .utils import Color
 
 
+def circle(
+    img: np.ndarray,
+    center: Point,
+    radius: int,
+    color: Union[int, Tuple[int, int, int]],
+    thickness: Optional[int] = None,
+):
+    cv.circle(img, (*center,), radius, color=color, thickness=thickness)
+
+
+def line(
+    image: np.ndarray,
+    point1: Point,
+    point2: Point,
+    color: Union[int, Tuple[int, int, int]],
+    thickness: int = 1,
+):
+    cv.line(image, (*point1,), (*point2,), color, thickness, cv.LINE_AA)
+
+
 def rectangle(
     image: np.ndarray,
     rectangle: Rect,
-    color: Union[Color, Tuple[int, ...]],
+    color: Union[Color, Tuple[int, ...], int],
     thickness: int = 1,
 ):
     if isinstance(color, Color):
