b92991b2a1f860a5362d6c0f857a8c0689be1749
Andreas Bergem <bergem.andreas@gmail.com>
2019-02-01 08:17:55 +0100

    Add Color enum as an option for all display functions.

--- cvhelper/display.py
+++ cvhelper/display.py
@@ -11,10 +11,12 @@ def circle(
     img: np.ndarray,
     center: Union[Point, Tuple[int, int]],
     radius: int,
-    color: Union[int, Tuple[int, int, int]],
+    color: Union[int, Tuple[int, int, int], Color],
     thickness: int = 1,
 ):
     x, y = map(int, center)
+    if isinstance(color, Color):
+        color = color.value
     cv.circle(img, (x, y), radius, color=color, thickness=thickness)
 
 
@@ -22,9 +24,11 @@ def line(
     image: np.ndarray,
     point1: Union[Point, Tuple[int, int]],
     point2: Union[Point, Tuple[int, int]],
-    color: Union[int, Tuple[int, int, int]],
+    color: Union[int, Tuple[int, int, int], Color],
     thickness: int = 1,
 ):
+    if isinstance(color, Color):
+        color = color.value
     cv.line(image, (*point1,), (*point2,), color, thickness, cv.LINE_AA)
 
 
@@ -43,17 +47,19 @@ def put_text(
     img: np.ndarray,
     text: str,
     origin: Union[Point, Tuple[int, int]],
-    color: Color = Color.RED,
+    color: Union[int, Tuple[int, int, int], Color] = Color.RED,
     thickness: Union[int, float] = 1,
     scale: Union[int, float] = 1,
-) -> None:
+):
+    if isinstance(color, Color):
+        color = color.value
     cv.putText(
         img,
         text,
         (*origin,),
         cv.FONT_HERSHEY_SIMPLEX,
         scale,
-        color.value,
+        color,
         thickness=thickness,
     )
 
