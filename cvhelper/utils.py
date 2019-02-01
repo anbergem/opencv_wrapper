24ee7cc8052398f3ae64c8f951acd84b8bfcbfb1
Andreas Bergem <bergem.andreas@gmail.com>
2019-02-01 08:18:13 +0100

    Add RANDOM in the Color enum.

--- cvhelper/utils.py
+++ cvhelper/utils.py
@@ -1,7 +1,20 @@
 import enum
 
+import numpy as np
 
-class Color(enum.Enum):
+
+class ColorAttr(enum.EnumMeta):
+    """
+    See:
+    https://stackoverflow.com/questions/47353555/how-to-get-random-value-of-attribute-of-enum-on-each-iteration/47353856
+    """
+
+    @property
+    def RANDOM(self):
+        return (*np.random.randint(0, 256, size=3),)
+
+
+class Color(enum.Enum, metaclass=ColorAttr):
     RED = (0, 0, 155)
     BLUE = (155, 0, 0)
     GREEN = (0, 155, 0)
