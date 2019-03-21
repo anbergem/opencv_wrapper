905b8e4c2cd131022d6c618a5496653e7fc2afbd
Andreas Bergem <bergem.andreas@gmail.com>
2019-03-21 08:55:08 +0100

    Add documentation to display functions.

--- cvhelper/display.py
+++ cvhelper/display.py
@@ -21,6 +21,15 @@ def circle(
     color: Union[int, Tuple[int, int, int], Color],
     thickness: int = 1,
 ):
+    """
+    Draw a circle on `img` at `center` with `radius`.
+
+    :param img: The image to draw the circle
+    :param center: The center at which to draw the circle
+    :param radius: The radius of the circle
+    :param color: The color of the circle.
+    :param thickness: The thickness of the circle; can be -1 to fill the circle.
+    """
     x, y = map(int, center)
     color = _ensure_compatible_color(color)
     cv.circle(img, (x, y), radius, color=color, thickness=thickness)
@@ -34,6 +43,17 @@ def line(
     thickness: int = 1,
     line_style: LineStyle = LineStyle.SOLID,
 ):
+    """
+    Draw a line from `point1` to `point2` on `image`.
+
+    :param image: The image to draw the line
+    :param point1: The starting point
+    :param point2: The ending point
+    :param color: The color of the line
+    :param thickness: The thickness of the line
+    :param line_style: The line style to draw. For LineStyle.DASHED, only thickness
+                       1 is currently supported.
+    """
     if isinstance(point1, tuple) or isinstance(point1.x, float):
         point1 = Point(*map(int, point1))
     if isinstance(point2, tuple) or isinstance(point2.x, float):
@@ -58,6 +78,16 @@ def rectangle(
     thickness: int = 1,
     line_style: LineStyle = LineStyle.SOLID,
 ):
+    """
+    Draw a rectangle on `image`.
+
+    :param image: The image to draw the rectangle
+    :param rect: The rectangle to be drawn
+    :param color: The color of the rectangle
+    :param thickness: The thickness of the lines; can be -1 to fill the rectangle.
+    :param line_style: The line style to draw. For LineStyle.DASHED, only thickness
+                       1 is currently supported.
+    """
     color = _ensure_compatible_color(color)
     rect = Rect(*map(int, rect))
     if line_style is LineStyle.SOLID:
@@ -79,6 +109,17 @@ def put_text(
     thickness: int = 1,
     scale: float = 1,
 ):
+    """
+    Put `text` on `image` at `origin`.
+
+    :param img: The image to draw the text
+    :param text: The text to be drawn
+    :param origin: The origin to start the text. The bottom of the first character
+                   is set in the origin.
+    :param color: The color of the text
+    :param thickness: The thickness of the text
+    :param scale: The scale of the text.
+    """
     color = _ensure_compatible_color(color)
     cv.putText(
         img,
@@ -92,4 +133,13 @@ def put_text(
 
 
 def wait_key(delay: int) -> str:
+    """
+    Wait for a key event infinitely (if `delay` is 0) or `delay` amount of milliseconds.
+
+    An alias for cv.waitKey(delay) & 0xFF. See cv.waitKey(delay) for further documentation.
+
+    :param delay: Amount of milliseconds to wait, or 0 for infinitely.
+    :return: The key pressed. Comparison of the key pressed can be found by `ord(str)`. For example
+             `if wait_key(0) == ord('q'): continue`
+    """
     return cv.waitKey(delay) & 0xFF
