eb54e5f5d2bc6d7b6b43f7f912d0e5e257db70bd
Andreas Bergem <bergem.andreas@gmail.com>
2019-04-05 09:56:41 +0200

    Move library info to __version__.
    Makes it easier to import from docs etc.

--- opencv_wrapper/__init__.py
+++ opencv_wrapper/__init__.py
@@ -1,3 +1,5 @@
+from .__version__ import __author__, __author_email__, __license__
+from .__version__ import __title__, __description__, __url__, __version__
 from .display import *
 from .image_operations import *
 from .misc_functions import *
--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os.path
 import sys
 
 from setuptools import setup
@@ -10,17 +11,24 @@ requirements = ["numpy<=1.15.3", "opencv-python<=4.0.1"]
 if sys.version_info[1] == 6:
     requirements.append("dataclasses")
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+about = {}
+with open(os.path.join(here, "opencv_wrapper", "__version__.py"), "r") as f:
+    exec(f.read(), about)
+
+
 setup(
-    name="opencv-wrapper",
-    version="0.2.0",
-    packages=["opencv_wrapper"],
-    author="Andreas Bergem",
-    author_email="bergem.andreas@gmail.com",
-    description="A python wrapper for OpenCV",
-    license="MIT",
+    name=about["__title__"],
+    version=about["__version__"],
+    author=about["__author__"],
+    author_email=about["__author_email__"],
+    description=about["__description__"],
+    license=about["__license__"],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/anbergem/opencv_wrapper",
+    url=about["__url__"],
+    packages=["opencv_wrapper"],
     classifiers=[
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
